10b95029ff531b1b6c00a13432314e867d2512d1
Akash Desarda <desardaakash@gmail.com>
2020-09-01 11:19:02 +0530

    added momentum with ema

--- stock_analysis_cli.py
+++ stock_analysis_cli.py
@@ -15,7 +15,9 @@ if task == 1:
     sa = UnitStrategy(path=yaml_path)
 
     sub_task = int(
-        input("Choose sub task to perform: \n1.Momentum Strategy \nChoice: "))
+        input("Choose sub task to perform: \n1.Momentum Strategy\n2.Momentum with EMA \nChoice: "))
+    
+# Sub-task for Momentum Strategy    
     if sub_task == 1:
         sub_task_para = int(input("Choose input parameter nature \n1.Default --> i)Date = today, \
 ii)Top company count = 20 iii)Export path = Same folder iv)Verbosity (level of detail logging): Detail\
@@ -43,6 +45,40 @@ ii)Top company count = 20 iii)Export path = Same folder iv)Verbosity (level of d
                                  top_company_count=sub_task_para_count,
                                  export_path=sub_task_para_export,
                                  verbosity=sub_task_para_verbosity)
+
+# Sub-task for Momentum with EMA Strategy
+    elif sub_task == 2:
+        sub_task_para = int(input("Choose input parameter nature \n1.Default --> i)Date = today, \
+ii)Top company count = 20 iii)EMA candidate = 50,200 iv)Export path = Same folder iv)Verbosity (level of detail logging): Detail\
+\n2.Manual \nChoice: "))
+        if sub_task_para == 1:
+            sa.momentum_with_ema_strategy()
+        elif sub_task_para == 2:
+            logger.info(
+                "In Manual mode whereever you see 'default' then presing enter key will take default value")
+            sub_task_para_date = (
+                input("Input desired date (default: today)(eg: 01/06/2020): ") or 'today')
+            if (len(sub_task_para_date.strip()) == 10 or sub_task_para_date == 'today'):
+                sub_task_para_date = str(sub_task_para_date)
+            else:
+                raise ValueError(
+                    f"Given date: {sub_task_para_date} is invalid. Eg input: 01/06/2020")
+            sub_task_para_count = int(
+                input("Input top company count (Default:20): ") or 20)
+            sub_task_para_emacandidate1 = int(
+                input("Input desired first EMA candidate (default: 50): ") or 50)
+            sub_task_para_emacandidate2 = int(
+                input("Input desired second EMA candidate (default: 200): ") or 200)
+            sub_task_para_export = (input(
+                'Enter path to save result (eg: ./some_folder) (default: .  (to save in current working directory): ') or '.')
+            sub_task_para_verbosity = int(
+                input('Input Verbosity [level 0: Minimal, 1: Detail (default: 1)]: ') or 1)
+
+            sa.momentum_with_ema_strategy(end_date=sub_task_para_date,
+                                          top_company_count=sub_task_para_count,
+                                          ema_canditate=(sub_task_para_emacandidate1,sub_task_para_emacandidate2),
+                                          export_path=sub_task_para_export,
+                                          verbosity=sub_task_para_verbosity)
 
 # Task for Indicator
 elif task == 2:
