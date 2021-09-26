07351ce22ef04f6fe6bc652d0216a09a34f642e6
Akash Desarda <desardaakash@gmail.com>
2021-09-26 17:53:00 +0530

    chore: :arrow_up: upgrade streamlit

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import os
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'Stock analysis'
-REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '1.5'
+NAME = "Stock analysis"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "1.6"
 DESCRIPTION = "Perform variety of stock analysis"
 # What packages are required for this module to be executed?
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     REQUIRED = f.read().splitlines()
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -17,8 +17,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
@@ -26,18 +26,18 @@ except FileNotFoundError:
 about = {}
 if not VERSION:
     project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about["__version__"] = VERSION
 
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     python_requires=REQUIRES_PYTHON,
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     install_requires=REQUIRED,
