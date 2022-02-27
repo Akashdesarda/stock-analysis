10fb991d1736033febc23a09754474456bee2fe5
Akash Desarda <desardaakash@gmail.com>
2022-02-27 15:41:47 +0530

    feat(REST API): :rocket: added rest api support for crud ops on deta base

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from stock_analysis.momentum_strategy import MomentumStrategy
-from stock_analysis.indicator import Indicator
 from stock_analysis.api.request_models import *
+from stock_analysis.indicator import Indicator
+from stock_analysis.momentum_strategy import MomentumStrategy
+from stock_analysis.utils.helpers import create_chunks, deta_base_client
 
-app = FastAPI()
+app = FastAPI()  # FastAPI app init
 
-
+# REST API for running algo strategy
 @app.post("/api/momentum/relative-momentum/")
 def relative_momentum(input_response: RelativeMomentum):
     ms = MomentumStrategy(company_name=input_response.company)
@@ -15,7 +16,10 @@ def relative_momentum(input_response: RelativeMomentum):
         top_company_count=input_response.top_company_count,
         save=False,
     )
-    result = result.set_index("symbol").T
+    # NOTE - by default result is return as dataframe's index as keys so changing it to symbol
+    result = result.set_index(
+        "symbol"
+    ).T  # need to perform `transpose` to match expected output
     return result
 
 
@@ -28,7 +32,10 @@ def relative_momentum_ema(input_response: RelativeMomentumEMA):
         ema_canditate=input_response.ema_candidate,
         save=False,
     )
-    result = result.set_index("symbol").T
+    # NOTE - by default result is return as dataframe's index as keys so changing it to symbol
+    result = result.set_index(
+        "symbol"
+    ).T  # need to perform `transpose` to match expected output
     return result
 
 
@@ -41,7 +48,10 @@ def absolute_momentum_dma(input_response: AbsoluteMomentumDMA):
         cutoff=input_response.cutoff,
         save=False,
     )
-    result = result.set_index("symbol").T
+    # NOTE - by default result is return as dataframe's index as keys so changing it to symbol
+    result = result.set_index(
+        "symbol"
+    ).T  # need to perform `transpose` to match expected output
     return result
 
 
@@ -49,7 +59,10 @@ def absolute_momentum_dma(input_response: AbsoluteMomentumDMA):
 def volume_n_days(input_response: VolumeNDaysIndicator):
     ind = Indicator(company_name=input_response.company)
     result = ind.volume_n_days_indicator(duration=input_response.duration, save=False)
-    result = result.set_index("symbol").T
+    # NOTE - by default result is return as dataframe's index as keys so changing it to symbol
+    result = result.set_index(
+        "symbol"
+    ).T  # need to perform `transpose` to match expected output
     return result
 
 
@@ -61,7 +74,10 @@ def ema_indicator_short(input_response: EMAIndicator):
         cutoff_date=input_response.cutoff_date,
         save=False,
     )
-    result = result.set_index("symbol").T
+    # NOTE - by default result is return as dataframe's index as keys so changing it to symbol
+    result = result.set_index(
+        "symbol"
+    ).T  # need to perform `transpose` to match expected output
     return result
 
 
@@ -73,7 +89,10 @@ def ema_indicator_detail(input_response: EMAIndicator):
         cutoff_date=input_response.cutoff_date,
         save=False,
     )
-    result = result.set_index("symbol").T
+    # NOTE - by default result is return as dataframe's index as keys so changing it to symbol
+    result = result.set_index(
+        "symbol"
+    ).T  # need to perform `transpose` to match expected output
     return result
 
 
@@ -83,5 +102,48 @@ def ema_crossover_indicator(input_response: EMACrossoverIndicator):
     result = ind.ema_crossover_detail_indicator(
         ema_canditate=input_response.ema_candidate, save=False
     )
-    result = result.set_index("symbol").T
+    # NOTE - by default result is return as dataframe's index as keys so changing it to symbol
+    result = result.set_index(
+        "symbol"
+    ).T  # need to perform `transpose` to match expected output
     return result
+
+
+# REST API for performing CRUD ops on Deta Base db
+# NOTE - `get` can be used to read/fetch/pull based on just `key`
+@app.get("/api/db/get/")
+def db_get(input_response: DBGet):
+    db_client = deta_base_client(input_response.db_name)
+    return db_client.get(input_response.key)
+
+
+# NOTE - `fetch` can be used to retrive data based on Deta base compatible query
+@app.get("/api/db/fetch/")
+def db_fetch(input_response: DBFetch):
+    db_client = deta_base_client(input_response.db_name)
+    return db_client.fetch(input_response.query).items
+
+
+# NOTE - `put` can be used for insert as well as update
+@app.put("/api/db/put/")
+def db_put(input_response: DBPut):
+    db_client = deta_base_client(input_response.db_name)
+    # since data is list then we can use `put_many` to speedup the ops
+    if isinstance(input_response.data, list):
+        if len(input_response.data) <= 25:
+            db_client.put_many(input_response.data)
+        else:
+            # NOTE - Deta has hard limit of 25 for `put_many`. So need to create chunks of 25 element of
+            # data so that we can still use `put_many`
+            chunked_data = create_chunks(input_response.data, 25)
+            for unit_chunk in chunked_data:
+                db_client.put_many(unit_chunk)
+    else:
+        db_client.put(input_response.data)
+
+
+# NOTE - the `delete` ops take place at `key` level so only key is required
+@app.delete("/api/db/delete/")
+def db_delete(input_response: DBDelete):
+    db_client = deta_base_client(input_response.db_name)
+    return db_client.delete(input_response.key)
